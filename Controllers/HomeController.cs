b7f48c4830e2bfe4769a36da5d2ab9e42d0a179f
LavishT <lavishtalwar28@github.com>
2020-06-16 11:41:52 -0400

    Added an About Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Fushion.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
